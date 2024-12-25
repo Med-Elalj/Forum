f9b64ad35e0b9f1c960109ccd54e43641579cf7d
Mtawil <twlmed212@gmail.com>
2024-12-25 18:09:12 +0100

    Recover files

--- frontend/assets/js/script-post.js
+++ frontend/assets/js/script-post.js
@@ -1,3 +1,4 @@
+function x1(){  
 
 
 // for Favourite Button on post Page
@@ -66,3 +67,121 @@ comment_dislike.forEach(dislike => {
         
     })
 })
+}
+
+function CommentReactionEventListenner(){
+    const commentButtons = document.querySelectorAll('.commentReaction .like');
+    commentButtons.forEach(button => {
+        button.addEventListener('click', async (e) => {
+            const commentID = e.target.id;
+            const response = await fetch(`/LikeComment/${commentID}`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            });
+            const data = await response.json();
+            if (data.success) {
+                const likeCount = e.target.querySelector('span');
+                likeCount.innerText = data.likeCount;// [ 'likeCount' : 1, 'dislikeCount' : 0 ];
+            }
+        });
+    });
+    const dislikeButtons = document.querySelectorAll('.commentReaction .dislike');
+    dislikeButtons.forEach(button => {
+        button.addEventListener('click', async (e) => {
+            const commentID = e.target.id;
+            const response = await fetch(`/DislikeComment/${commentID}`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            });
+            const data = await response.json();
+            if (data.success) {
+                const dislikeCount = e.target.querySelector('span');
+                dislikeCount.innerText = data.dislikeCount;
+            }
+        });
+    });
+}
+
+function CommentInputEventListenner(){
+    console.log("Listx");
+
+    const commentInput = document.querySelector('.commentInput input');
+    console.log("List");
+    commentInput.addEventListener('keypress', async (e) => {
+        
+        console.log("Rak Hena ");
+        
+        if (e.key === 'Enter') {
+            console.log('Enter Pressed');
+            e.preventDefault();
+            const comment = commentInput.value;
+            commentInput.value = '';
+            const postID = commentInput.id;
+            const response = await fetch('/CreateComment', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    postID,
+                    comment
+                })
+            });
+            console.log("response", response);
+            
+            const data = await response.json();
+            console.log("Response  = data", data);
+            console.log("data stst", data["status"] == "ok");
+            console.log('Comment Added', (response.success));
+            if (data["status"] == "ok") {
+                const commentContainer = document.querySelector('.Comments');
+                const commentCard = document.createElement('div');
+                commentCard.classList.add('commentCard');
+                commentCard.classList.add('CommentAdded');
+                commentCard.id = "Comment" + data["CommentID"];
+                console.log(commentCard);
+                let url = "/index#Comment" + data["CommentID"];
+                commentCard.innerHTML = `
+                    <div class="commentAuthorImage">
+                        <img src="https://ui-avatars.com/api/?name=${data["UserName"]}" alt="">
+                    </div>
+                    <div class="commentAuthor">
+                        <div class="commentAuthorInfo">
+                            <span class="commentAuthorName">
+                                @${data["UserName"]}
+                                <span class="commentTime">
+                                    ${data["CreatedAt"]}
+                                </span>
+                            </span>
+                            <div class="commentReaction DisableUserSelect">
+                                <span class="like" id="${data["CommentID"]}">
+                                    <span class="material-symbols-outlined">
+                                        thumb_up
+                                    </span>
+                                    <span>0</span>
+                                </span>
+                                <span class="dislike" id="${data["CommentID"]}">
+                                    <span class="material-symbols-outlined">thumb_down</span>
+                                    <span>0</span>
+                                </span>
+                            </div>
+                        </div>
+                        <div class="commentInfo">
+                            <p class="commentData collapse">${data["Content"]}</p>
+                        </div>
+                    </div>
+                `;
+                commentContainer.prepend(commentCard);
+                window.location.replace(url)
+                CommentReactionEventListenner(); //Re-adding event listeners
+            }
+        }
+    });
+}
+CommentReactionEventListenner()
+CommentInputEventListenner()
+x1()
--- frontend/assets/js/script.js
+++ frontend/assets/js/script.js
@@ -1,97 +1,6 @@
-//###//////////////////// Start Listning dropDown Buttons ////////////
-const CreatePostModel = document.querySelector(".postModal")
-const CreatePostArea = document.querySelector(".new-post-header")
 let popup = NaN;
-const closeCreatePostModal = document.querySelector(".titleInput .close-post")
 
-// Get Left Sidebar to show it on mobile
-const sidebardLeft = document.querySelector('.sidebar-left')
-
-const CreatePostInputTitle = document.querySelector(".titleInput input")
-const dropdown = document.querySelectorAll('.dropdown i, .dropdown .ProfileImage')
-let contentList = document.querySelectorAll('.content')
-
-dropdown.forEach(drop => {
-
-    let contentSibling = drop.nextElementSibling
-    drop.addEventListener('click', () => {
-        contentSibling.classList.toggle("show")
-    })
-    document.addEventListener('click', function (event) {
-        if (!contentSibling.contains(event.target) && !drop.contains(event.target) && contentSibling.classList.contains("show")) {
-            console.log(contentSibling.classList);
-            contentSibling.classList.remove('show');
-        }
-    });
-})
-
-//###//////////////////// Start Listning on Like Dislike Buttons ////////////
-let like = document.querySelectorAll(".react .like")
-let dislike = document.querySelectorAll(".react .dislike")
-
-const popupHTML = `
-    <div id="popup" class="popup">
-        <div class="popup-content">
-            <h1>Thanks for trying</h1>
-            <p>Log in or sign up to add comments, likes, dislikes, and more.</p>
-            <a href="login.html"><button>Log in</button></a>
-            <a href="register.html"><button>Sign up</button></a>
-            <span class="logged-out">Stay logged out</span>
-        </div>
-    </div>
-`;
-
-
-const popUp = () => {
-    const popupContainer = document.getElementById("popupContainer")
-    popupContainer.innerHTML = popupHTML;
-    popup = document.getElementById('popup');
-
-    popup.style.display = 'flex';
-}
-
-window.onclick = function (event) {
-
-    if (event.target == CreatePostModel) {
-        CreatePostModel.style.display = "none"
-    } else if (event.target == popup) {
-        popup.style.display = "none"
-    }
-}
-// Handling Like Button Clicked in Post Comments
-like.forEach(like_elem => {
-    like_elem.addEventListener('click', function () {
-        // Check if the user is loggedin or not :
-        if (checkUserIsLogged()) {
-            // Check from DB if like or dislike exits
-            /// /// / // / / / / / /
-            let dislike_sibling = like_elem.nextElementSibling;
-            like_elem.classList.toggle("FILL");
-            dislike_sibling.classList.remove("FILL");
-        } else {
-            popUp();
-        }
-    })
-})
-
-dislike.forEach(dislike_elem => {
-    dislike_elem.addEventListener('click', function () {
-        if (checkUserIsLogged()) {
-            let like_sibling = dislike_elem.previousElementSibling;
-            dislike_elem.classList.toggle("FILL");
-            like_sibling.classList.remove("FILL");
-        } else {
-            popUp();
-        }
-
-
-        // the  rest of the code will write here to send request
-        // to backend to update database 
-
-    })
-})
-
-//###//////////////////// Check User is Logged yet or not //////////////////
+//////////////////// Check User is Logged yet or not //////////////////
 function checkUserIsLogged() {
     //const test = localStorage.setItem("token", "This is a test token")
     const token = localStorage.getItem("token")
@@ -100,29 +9,127 @@ function checkUserIsLogged() {
     return token !== null && token !== undefined;
 }
 
-//###//////////////////// See more Option  ////////////
-document.querySelectorAll('.see-more').forEach(tweetText => {
-    const seeMoreLink = tweetText;
+//////////////////// Popup function //////////////////
+const popUp = () => {
+    const popupContainer = document.getElementById("popupContainer")
+    const popupHTML = `
+        <div id="popup" class="popup">
+            <div class="popup-content">
+                <h1>Thanks for trying</h1>
+                <p>Log in or sign up to add comments, likes, dislikes, and more.</p>
+                <a href="login.html"><button>Log in</button></a>
+                <a href="register.html"><button>Sign up</button></a>
+                <span class="logged-out">Stay logged out</span>
+            </div>
+        </div>
+    `;
+    popupContainer.innerHTML = popupHTML;
+    popup = document.getElementById('popup');
 
-    // Only show 'See More' if text is actually truncated
-    const paragraph = tweetText.previousElementSibling.querySelector('p');
-    if (paragraph.scrollHeight <= 50) {
-        seeMoreLink.style.display = 'none';
-    }
+    popup.style.display = 'flex';
+}
+//////////////// Start Listning dropDown List For Posts ////////////
+function postControlList() {
+    
+    const dropdown = document.querySelectorAll('.dropdown i, .dropdown .ProfileImage')
 
-    seeMoreLink.addEventListener('click', () => {
-        tweetText.previousElementSibling.classList.toggle('expanded');
+    dropdown.forEach(drop => {
 
-        // Toggle see more/see less text
-        seeMoreLink.textContent = tweetText.previousElementSibling.classList.contains('expanded')
-            ? 'See Less'
-            : 'See More';
+        let contentSibling = drop.nextElementSibling
+        drop.addEventListener('click', () => {
+            contentSibling.classList.toggle("show")
+        })
+        document.addEventListener('click', function (event) {
+            if (!contentSibling.contains(event.target) && !drop.contains(event.target) && contentSibling.classList.contains("show")) {
+                contentSibling.classList.remove('show');
+            }
+        });
+    })
+} 
+//////////////////// Menu Icon On header PAGE Burger Icon for Mobile //////////
+
+function showLeftSidebarMobile(){
+    const sidebardLeft = document.querySelector('.sidebar-left')
+    const menuIcon = document.querySelector('.menu')
+    menuIcon.addEventListener('click', () => {
+        sidebardLeft.style.left = sidebardLeft.style.left === '0px' ? '-100%' : '0px'
+    })
+
+    // Hiding Humberger Menu on Mobile if the user change the view port
+    const x = window.matchMedia("(min-width: 768px)")
+    x.addEventListener('change', (e) => {
+        if (e.matches) {
+            sidebardLeft.style.left = '2.5%'
+        } else {
+            sidebardLeft.style.left = '-100%'
+        }
+    })
+}
+
+
+//////////////////// Start Listning on Like Dislike Buttons ////////////
+function LikeAndDeslikePostButtons() {
+    let like = document.querySelectorAll(".react .like")
+    let dislike = document.querySelectorAll(".react .dislike")
+    // Handling Like Button Clicked in Post Comments
+    like.forEach(like_elem => {
+        like_elem.addEventListener('click', function () {
+            // Check if the user is loggedin or not :
+            if (checkUserIsLogged()) {
+                // Check from DB if like or dislike exits
+                /// /// / // / / / / / /
+                let dislike_sibling = like_elem.nextElementSibling;
+                like_elem.classList.toggle("FILL");
+                dislike_sibling.classList.remove("FILL");
+            } else {
+                popUp();
+            }
+        })
+    })
+    
+    dislike.forEach(dislike_elem => {
+        dislike_elem.addEventListener('click', function () {
+            if (checkUserIsLogged()) {
+                let like_sibling = dislike_elem.previousElementSibling;
+                dislike_elem.classList.toggle("FILL");
+                like_sibling.classList.remove("FILL");
+            } else {
+                popUp();
+            }
+            // the  rest of the code will write here to send request
+            // to backend to update database 
+    
+        })
+    })
+    
+}
+
+
+//////////////////// See more Option  ////////////
+function seeMore() {
+    document.querySelectorAll('.see-more').forEach(tweetText => {
+        const seeMoreLink = tweetText;
+    
+        // Only show 'See More' if text is actually truncated
+        const paragraph = tweetText.previousElementSibling.querySelector('p');
+        if (paragraph.scrollHeight <= 50) {
+            seeMoreLink.style.display = 'none';
+        }
+    
+        seeMoreLink.addEventListener('click', () => {
+            tweetText.previousElementSibling.classList.toggle('expanded');
+    
+            // Toggle see more/see less text
+            seeMoreLink.textContent = tweetText.previousElementSibling.classList.contains('expanded')
+                ? 'See Less'
+                : 'See More';
+        });
     });
-});
-
-//###////////////////////   Listening on user request of post  /////
+}
+////////////////////   Listening on user request of post  /////
 // ///To Read Post We need to make request to backend, to get Full Page to Display it to the user
-
+function readPost() {
+   
 async function fetchPost(url) {
     try {
         //?id=${idValueFromClikedArea}
@@ -157,41 +164,13 @@ postButton.forEach(elem => {
                 postContent.classList.add("closed")
                 //restore the scrolling on the background page :D 
                 document.body.classList.remove("stop-scrolling");
+                document.getElementById("ScriptInjected").remove()
             }
         })
     })
 })
 
-
-
-//###////////////////////  Creat NEW POST Listenner  //////////
-
-CreatePostArea.addEventListener('click', () => {
-    CreatePostModel.style.display = "flex"
-    CreatePostInputTitle.focus()
-    closeCreatePostModal.addEventListener('click', () => {
-        CreatePostModel.style.display = "none"
-    })
-})
-
-
-//###//////////////////// Menu Icon On header PAGE Burger Icon for Mobile //////////
-
-const menuIcon = document.querySelector('.menu')
-menuIcon.addEventListener('click', () => {
-    sidebardLeft.style.left = sidebardLeft.style.left === '0px' ? '-100%' : '0px'
-})
-
-// Hiding Humberger Menu on Mobile if the user change the view port
-const x = window.matchMedia("(min-width: 768px)")
-x.addEventListener('change', (e) => {
-    if (e.matches) {
-        sidebardLeft.style.left = '2.5%'
-    } else {
-        sidebardLeft.style.left = '-100%'
-    }
-})
-
+}
 // Dark Mode && save token in local storage
 const themeToggle = document.getElementById('switch');
 const body = document.body;
@@ -204,6 +183,8 @@ function toggleDarkMode(isDark) {
 }
 const darkModeStored = localStorage.getItem('darkMode') === 'true';
 toggleDarkMode(darkModeStored);
+console.log("test",darkModeStored);
+
 themeToggle.checked = darkModeStored;
 themeToggle.addEventListener('change', (e) => toggleDarkMode(e.target.checked));
 
@@ -222,3 +203,115 @@ window.addEventListener('hashchange', () => {
         section.style.display  === '#posts' ? 'block' : 'none';
     });
 });
+
+
+function createPost() {
+    const CreatePostArea = document.querySelector(".new-post-header")
+    const CreatePostModel = document.querySelector(".postModal")
+    const closeCreatePostModal = document.querySelector(".titleInput .close-post")
+    
+    window.onclick = function (event) {
+        if (event.target == CreatePostModel) {
+            CreatePostModel.style.display = "none"
+        } else if (event.target == popup) {
+            popup.style.display = "none"
+        }
+    }
+
+    const CreatePostInputTitle = document.querySelector(".titleInput input")
+    CreatePostArea.addEventListener('click', () => {
+        CreatePostModel.style.display = "flex"
+        CreatePostInputTitle.focus()
+        closeCreatePostModal.addEventListener('click', () => {
+            CreatePostModel.style.display = "none"
+        })
+    })
+
+    const form = document.querySelector('.CreatePostContainer form');
+    form.addEventListener('submit', async (e) => {
+        e.preventDefault();
+        const title = form.title.value;
+        const content = form.content.value;
+        const categories = Array.from(form.category).filter((input) => input.checked).map((input) => input.id);
+        const data = {
+            title,
+            content,
+            categories
+        };
+        console.log("data", data);
+        
+        const response = await fetch('/createPost', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(data),
+        });
+        console.log(response.status );
+        
+        if (response.status === 200) {
+            
+            const post = await response.json();
+            const postCard = document.createElement('div');
+            postCard.classList.add('post-card');
+            postCard.innerHTML = `
+                <div class="ProfileImage tweet-img" style="background-image: url('https://ui-avatars.com/api/?name=${post.UserName}')"></div>
+                <div class="post-details">
+                    <div class="row-tweet">
+                        <div class="post-header">
+                            <span class="tweeter-name post" id="${post.ID}">
+                                ${post.Title}
+                                <br><span class="tweeter-handle">@${post.UserName} ${post.CreatedAt}.</span>
+                            </span>
+                        </div>
+                    </div>
+                    <div class="post-content">
+                        <p>${post.Content}</p>
+                    </div>
+                    <span class="see-more">See More</span>
+                    <div class="Hashtag">
+                        ${post.Categories.map((category) => `<a href=""><span>#${category}</span></a>`).join('')}
+                    </div>
+                    <div class="post-footer">
+                        <div class="react">
+                            <div class="counters like" id="${post.ID}">
+                                <i class="material-symbols-outlined popup-icon">thumb_up</i>
+                                <span>${post.LikeCount}</span>
+                            </div>
+                            <div class="counters dislike" id="${post.ID}">
+                                <i class="material-symbols-outlined popup-icon">thumb_down</i>
+                                <span>${post.DislikeCount}</span>
+                            </div>
+                        </div>
+                        <div class="comment post" id="${post.ID}">
+                            <i class="material-symbols-outlined showCmnts">comment</i>
+                            <span>0</span>
+                        </div>
+                    </div>
+                </div>
+            `;
+            // insert PostCard inside the main-feed and exact after first child of main-feed = new-tweet
+            const mainFeed = document.querySelector('.main-feed');
+            postCard.classList.add('PostAdded');
+            mainFeed.insertBefore(postCard, mainFeed.children[1]);
+
+            CreatePostModel.style.display = "none";
+            form.reset();
+            // Recall Function To append new post to their Lestining Buttons
+            LikeAndDeslikePostButtons()
+            seeMore()
+            postControlList()
+            readPost() 
+
+            // window.location.href = `/post/${post.ID}`;
+        } else {
+            alert("Error Creating Post");
+        }
+    });
+}
+createPost();/// Check user is logged before running this Function TODO
+postControlList()
+readPost() 
+showLeftSidebarMobile()
+LikeAndDeslikePostButtons()
+seeMore()
